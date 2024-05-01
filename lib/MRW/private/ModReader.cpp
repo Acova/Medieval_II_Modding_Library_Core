925ed6d6852472fbfedb35402f7e32984ef95203
Hector Acosta Valido <hector.acosta.valido@outlook.es>
2024-05-01 12:01:59 +0000

    Added basic reading file system

--- lib/MRW/private/ModReader.cpp
+++ lib/MRW/private/ModReader.cpp
@@ -1,7 +1,19 @@
 #include "ModReader.h"
+#include <fstream>
 #include <iostream>
+#include <filesystem>
 
 void IModReader::readFile(std::string filePath)
 {
     std::cout << "Hola mundo 2\n";
+    std::ifstream file("../lib/MRW/resources/export_descr_buildings.txt");
+    std::string line;
+    if (file.is_open()) {
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+        
+    } else {
+        std::cerr << "Unable to open file!" << std::endl;
+    }
 }
