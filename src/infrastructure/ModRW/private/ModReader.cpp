9a71568e2da6165eb5c84d5587929d765e2dc26b
Hector Acosta Valido <hector.acosta.valido@outlook.es>
2024-05-01 18:50:56 +0000

    Continued the refactorization

--- src/infrastructure/ModRW/private/ModReader.cpp
+++ src/infrastructure/ModRW/private/ModReader.cpp
@@ -6,7 +6,7 @@
 void IModReader::readFile(std::string filePath)
 {
     std::cout << "Hola mundo 2\n";
-    std::ifstream file("../lib/MRW/resources/export_descr_buildings.txt");
+    std::ifstream file("../src/infrastructure/ModRW/resources/export_descr_buildings.txt");
     std::string line;
     if (file.is_open()) {
         while (std::getline(file, line)) {
