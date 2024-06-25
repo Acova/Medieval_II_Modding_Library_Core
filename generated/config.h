750c057b29f749ecce56003ff8f712b4236de4ae
Hector Acosta Valido <hector.acosta.valido@outlook.es>
2024-06-25 07:02:03 +0000

    Moved the resources outside the sources of the project

--- generated/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-char* DEFINITIONS_DATABASE_FILEPATH = "/opt/projects/medieval-ii-modding-library-core/src/infrastructure/DefinitionsLoader/resources/miimlc.db";
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include "ModReader.h"
-#include "config.h"
 
 int main() {
-    std::string filePath = DEFINITIONS_DATABASE_FILEPATH;
-    ModReader* modReader = new ModReader("/opt/projects/medieval-ii-modding-library-core/src/infrastructure/DefinitionsLoader/resources/miimlc.db");
+    ModReader* modReader = new ModReader("/opt/projects/medieval-ii-modding-library-core/resources/miimlc.db");
     return 0;
 }
